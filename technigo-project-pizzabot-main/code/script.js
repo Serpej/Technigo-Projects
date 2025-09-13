cdfdb615733e28a7f78f587712e489107d22da0c
Serpej <jehag@live.se>
2025-09-13 14:50:37 +0200

    Add logic for foodchoice and subtype choice

--- technigo-project-pizzabot-main/code/script.js
+++ technigo-project-pizzabot-main/code/script.js
@@ -10,7 +10,7 @@ alert(
 );
 
 // Step 2 - Food choice
-const foodChoice = prompt(
+const foodMenu = prompt(
   `What kind of food would you like to order?
   Enter a number:
   1 - Pizza
@@ -18,23 +18,82 @@ const foodChoice = prompt(
   3 - Salad`
 );
 
+let foodChoice = "";
 
-if(foodChoice === `1`) {
-  alert (`1`);
-
-} else if (foodChoice === `2`) {
-  alert(`2`);
-
-} else if (foodChoice === `3`) {
-  alert (`3`);
-
+if(foodMenu === `1`) {
+  alert (`You chose pizza.`);
+  foodChoice = `Pizza`;
+} else if (foodMenu === `2`) {
+  alert(`You chose pasta.`);
+  foodChoice = `Pasta`;
+} else if (foodMenu === `3`) {
+  alert (`You chose salad`);
+  foodChoice = `Salad`;
 } else {
   alert(`Invalid input, you have to choose a number between 1 and 3`);
 }
 
-
 // Step 3 - Subtype choice
-// Your code goes here
+
+let subTypeChoice = "";
+
+ if(foodChoice === `Pizza`) {
+    subTypeMenu = prompt(
+      `What pizza would you like to order.
+      Enter a number:
+      1 - Hawaiian
+      2 - Quattro Straggione
+      3 - Vegetale`
+    );
+    if (subTypeMenu === `1`) {
+      subTypeChoice = `Hawaiian`;
+      alert(`You chose ${subTypeChoice}`);
+    } else if (subTypeMenu === `2`) {
+      subTypeChoice = `Quattro Straggione`;
+      alert(`You chose ${subTypeChoice}`);
+    } else if (subTypeMenu === `3`) {
+      subTypeChoice = `Vegetale`;
+      alert(`You chose ${subTypeChoice}`);
+    }
+ } else if(foodChoice === `Pasta`) {
+    subTypeMenu = prompt(
+      `What pizza would you like to order.
+      Enter a number:
+      1 - Bolognese
+      2 - Carabonara
+      3 - Pasta Pesto`
+    );
+    if (subTypeMenu === `1`) {
+      subTypeChoice = `Bolognese`;
+      alert(`You chose ${subTypeChoice}`);
+    } else if (subTypeMenu === `2`) {
+      subTypeChoice = `Carabonara`;
+      alert(`You chose ${subTypeChoice}`);
+    } else if (subTypeMenu === `3`) {
+      subTypeChoice = `Pasta Pesto`;
+      alert(`You chose ${subTypeChoice}`);
+    }
+ } else if(foodChoice === `Salad`) {
+    subTypeMenu = prompt(
+      `What pizza would you like to order.
+      Enter a number:
+      1 - Greek Style
+      2 - Asian Style
+      3 - Pasta Salad`
+    );
+    if (subTypeMenu === `1`) {
+      subTypeChoice = `Greek Style`;
+      alert(`You chose ${subTypeChoice}`);
+    } else if (subTypeMenu === `2`) {
+      subTypeChoice = `Asian Style`;
+      alert(`You chose ${subTypeChoice}`);
+    } else if (subTypeMenu === `3`) {
+      subTypeChoice = `Pasta Salad`;
+      alert(`You chose ${subTypeChoice}`);
+    }
+ } else {
+    alert(`Invalid input, you have to choose a number between 1 and 3`);
+ }
 
 // Step 4 - Age
 // Your code goes here
