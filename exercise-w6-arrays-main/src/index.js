9a433f80ac50b3afa4af1311a5196b7e93fc838a
Serpej <jehag@live.se>
2025-09-21 16:10:33 +0200

    Add new exercises in arrays. Halfway through.

---
+++ exercise-w6-arrays-main/src/index.js
@@ -0,0 +1,100 @@
+const pokemons = [
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Metapod",
+  "Weedle",
+  "Pikachu",
+  "Pidgey"
+];
+
+/* const countThem = () => {
+  // print out, in a sentence, how many pokemons I have.
+  // like: "I have x pokemons!"
+}; */
+
+const countThem = () => {
+ let numOfpokemons = pokemons.length;
+
+ console.log(`I have ${numOfpokemons} Pokémons`);
+}
+
+const orderThem = () => {
+  // order the pokemons alphabetically
+  //let pokemonsSorted = pokemons.sort();
+  pokemons.sort();
+  console.log(pokemons);
+};
+orderThem();
+
+const flipThem = () => {
+  // reverse the order of the pokemons
+  pokemons.reverse();
+  console.log(pokemons);
+};
+flipThem();
+
+const makeThemBig = () => {
+  // print the pokemons in UPPERCASE letters
+  for (let i = 0; i < pokemons.length; i++) {
+    pokemons[i] = pokemons[i].toUpperCase();
+  }
+  console.log(pokemons);
+};
+makeThemBig();
+
+
+const onlyTheBs = () => {
+  // only print the pokemons that starts with B
+  
+  const result = pokemons.filter((element) => {
+   return element.toLowerCase().startsWith("b");
+  });
+  console.log(result);
+};
+onlyTheBs();
+
+const notTheCs = () => {
+  // remove all pokemons that starts with C
+
+  const result = pokemons.filter( element => {
+    return !element.toLowerCase().startsWith("c")
+  });
+  console.log(result);
+};
+notTheCs();
+
+
+const nameAndIdThanks = () => {
+  // print out name and index of all pokemons
+  // like: number x - Squirtle
+  for (let i = 0; i < pokemons.length; i++) {
+    console.log(`Number ${[i]} is ${pokemons[i]}`);
+  };
+};
+nameAndIdThanks();
+
+const catchPokemon = name => {
+  // add a pokemon with a name of your choice to the list,
+  // print the list so you see its there.
+};
+
+const didICatchIt = name => {
+  // check the pokemons to see if a specific pokemon is in the array
+};
+
+const addInThirdPlace = () => {
+  // add the pokemon "Clefairy" in the third place of the array
+  // print the list so you see its there.
+};
+
+// ***BONUS***
+const theLongestName = () => {
+  // find the pokemon with the longest name
+};
