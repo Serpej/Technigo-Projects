0e3af4c6362cf88c3870c1caea601f6c77a54fcb
Serpej <jehag@live.se>
2025-10-21 20:39:57 +0200

    Add future days as weekdays in an UL

--- technigo-project-weather-app-main/script.js
+++ technigo-project-weather-app-main/script.js
@@ -3,20 +3,21 @@ const todaysTemperatureContainer = document.getElementById("temp-container");
 const cityContainer = document.getElementById("city-container");
 const weatherDescriptionContainer = document.getElementById("description-container");
 const sunriseSunsetContainer = document.getElementById("Sunrise-Sunset-container");
+const futureDaysContainer = document.getElementById("future-forecast-container");
+const dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 
 roundToInteger = (number) => {
   return Math.round(number);
 };
 
-const fetchData =  async () => {
+const fetchDataTodaysForecast =  async () => {
   try {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=798eeac13b738f9ebfbf936c1e8564a9");
     if(!response.ok) {
       throw new Error(`HTTP Error! Status: ${response.status}`);
     };
     const data = await response.json();
-    console.log(data);
 
     // Get todays temp rounded
     const todaysTemperature = document.createElement("span");
@@ -60,9 +61,37 @@ const fetchData =  async () => {
   }
 
 }
-fetchData();
+fetchDataTodaysForecast();
 
-const sunriseTime = 1760938645 * 1000;
-const sunrise = new Date(sunriseTime);
 
-console.log(sunrise);
+const fetchDataNextFiveDaysForecast = async () => {
+  try {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=798eeac13b738f9ebfbf936c1e8564a9");
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    const eachDay = Array.from(data.list);
+
+    const daysList = document.createElement("ul");
+    daysList.id = "futureDayList";
+
+    eachDay.forEach((day) =>{
+      const time = day.dt_txt;
+
+      if (time.includes("12:00:00")) {
+        dateOfTime = new Date(time);
+        day = dateOfTime.getDay();
+      
+        const futureDay = document.createElement("li");
+        futureDay.textContent = `${dayNames[day]}`;
+
+        daysList.appendChild(futureDay);
+      }
+    });
+    futureDaysContainer.appendChild(daysList);
+  } catch (error) {
+    console.log(`Error occured!`, error);
+  }
+}
+fetchDataNextFiveDaysForecast()
