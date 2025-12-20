af7231c28d848d39576d5e6f9ae0cc4b7b5ac892
Serpej <jehag@live.se>
2025-12-20 11:43:35 +0100

    Edit background color of featured images

--- technigo-project-portfolio-main/src/components/HeaderImages.jsx
+++ technigo-project-portfolio-main/src/components/HeaderImages.jsx
@@ -1,12 +1,14 @@
-import data from "../data.json"
+import data from "../data.json";
 
 export const HeaderImages = () => {
   const { projects } = data;
   const featuredProjectImages = projects.map((project, i) => {
     if (i < 3) {
+      console.log(project.image)
       return <img id={`image${i}`}
         key={i}
         src={project.image}
+        alt= {`Project image number ${i + 1}`}
         />
     };
   });
