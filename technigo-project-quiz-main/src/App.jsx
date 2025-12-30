ae9b57462c7c3dc5597f3545eaf07a488f573ac1
Serpej <jehag@live.se>
2025-12-30 13:56:29 +0100

    Add clickhandler and changed statement data
    ...into objects to add unique keys. I will use
    these to track each color choice.

--- technigo-project-quiz-main/src/App.jsx
+++ technigo-project-quiz-main/src/App.jsx
@@ -1,5 +1,4 @@
 import { QuestionHeader } from "./components/QuestionHeader"
-import { QuestionStatements } from "./components/QuestionStatements"
 import data from './data.json'
 import {useState} from "react"
 
@@ -9,20 +8,27 @@ export const App = () => {
 
 const [index, setIndex] = useState(0);
 
+// Destructure the questions array from data
+const { questions } = data;
+let questionArray = questions[index];
 
-const { questions } = data
-let questionArray = questions[index]
+// Clickhandler
+const handleClick = (colorChoice) => {
+  handleColorChoice(colorChoice);
+  setIndex(index + 1);
+};
 
-const statementArray = questionArray.statements.map((statement) => {
+const statementsArray = questionArray.statements.map((statement) => {
+  let colorChoice = statement.color;
   return (
-    <p key={statement}>{statement}</p>
+    <p key={statement.key} className="statementParagraph" onClick={ ({colorChoice}) => handleClick}>{colorChoice}</p>
   )
 })
 
   return (
     <div>
       <QuestionHeader header={questionArray.header} />
-      {statementArray}
+      {statementsArray}
     </div>
   )
-}
+};
