6b82e826947b3018f66e5b017329497c7b0b1277
Serpej <jehag@live.se>
2025-12-30 15:17:51 +0100

    Add a function to handleColorChoice
    By updating a useState hook for counting colors

--- technigo-project-quiz-main/src/App.jsx
+++ technigo-project-quiz-main/src/App.jsx
@@ -7,22 +7,48 @@ import {useState} from "react"
 export const App = () => {
 
 const [index, setIndex] = useState(0);
+const [colorCounts, setColorCounts] = useState([
+  { key: "Blue", count: 0},
+  { key: "Green", count: 0},
+  { key: "Black", count: 0},
+  { key: "White", count: 0},
+  { key: "Red", count: 0},
+  { key: "Colorless", count: 0}
+])
 
 // Destructure the questions array from data
 const { questions } = data;
 let questionArray = questions[index];
 
+
+// A state function updater. It updates the count of the useState Array with objects.
+const handleColorChoice = (colorKey) => {
+  setColorCounts( previousStateReactArray => {
+    const nextStateReactArray = previousStateReactArray.map(color => {
+      const updated = color.key === colorKey ? {...color, count: color.count + 1} : color;
+      return updated
+    })
+
+    const selected = nextStateReactArray.find(c => c.key === colorKey)
+
+    if (selected) {
+    console.log(`Key changed, ${selected.key} is now ${selected.count}`)
+    }
+    
+    return nextStateReactArray;
+  })
+};
+
 // Clickhandler
-const handleClick = (colorChoice) => {
-  handleColorChoice(colorChoice);
+const handleClick = (colorKey) => {
+  handleColorChoice(colorKey);
   setIndex(index + 1);
 };
 
 const statementsArray = questionArray.statements.map((statement) => {
-  let colorKey = statement.key;
-  console.log(colorKey);
+  const colorKey = statement.key;
   return (
-    <p key={colorKey} className="statementParagraph" onClick={ ({colorKey}) => handleClick}>{statement.color}</p>
+    <p key={colorKey} className="statementParagraph" onClick={() => handleClick(colorKey)}>{statement.color}</p>
   )
 })
 
@@ -33,3 +59,4 @@ const statementsArray = questionArray.statements.map((statement) => {
     </div>
   )
 };
+
