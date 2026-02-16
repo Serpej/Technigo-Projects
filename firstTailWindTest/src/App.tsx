7c8e181a7ad398b6b38c397a8b008272e210c433
Serpej <jehag@live.se>
2026-02-16 13:52:52 +0100

    Add my first tailWind test project

---
+++ firstTailWindTest/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='container mx-auto flex justify-center'>
+        <h1 className='text-2xl text-center font-bold text-green-800'>Hello TailWindCss with React and Vite</h1>
+      </div>
+  )
+}
+
+export default App
