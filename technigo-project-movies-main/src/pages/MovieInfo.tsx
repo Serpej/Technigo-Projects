a69e88f81033609c3bddebe7010c3b8c02256933
Serpej <jehag@live.se>
2026-02-28 15:45:40 +0100

    Add border to image, padding and line height to p

--- technigo-project-movies-main/src/pages/MovieInfo.tsx
+++ technigo-project-movies-main/src/pages/MovieInfo.tsx
@@ -17,19 +17,24 @@ export const MovieInfo= () => {
   }, [id]);
 
   const chosenMovie = movie;
-  const posterPath = `https://image.tmdb.org/t/p/w300${chosenMovie?.poster_path}`;
+  const posterPath = `https://image.tmdb.org/t/p/w342${chosenMovie?.poster_path}`;
   const backgroundImagePath = `url(https://image.tmdb.org/t/p/w1280${chosenMovie?.backdrop_path})`;
 
   return  (
     <div 
-      className="min-h-screen bg-cover"
+      className="flex flex-col justify-between min-h-screen bg-cover"
       style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), ${backgroundImagePath}`}}
     >
       <Link to="/"><button type="button">Back</button></Link>
-      <div >
-        <img src={posterPath} alt="image of the chosen movie" />
-        <h1 className="text-3xl text-white font-bold">{chosenMovie?.title}</h1>
-        <p className="text-white">{chosenMovie?.overview}</p>          
+      <div className="flex p-[50px]">
+        <img 
+          src={posterPath} 
+          alt="image of the chosen movie" 
+          className="border-[5px] border-white"/>
+        <div className="flex flex-col ml-5 justify-end">
+          <h1 className="text-3xl text-white font-bold">{chosenMovie?.title}</h1>
+          <p className="text-white my-[16px] max-w-[400px] leading-0">{chosenMovie?.overview}</p>
+        </div>        
       </div>
     </div>
   )
