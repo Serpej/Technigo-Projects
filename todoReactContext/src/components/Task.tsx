bd9041f515c2da90e1dfed11b39ea677882cd7a0
Serpej <jehag@live.se>
2026-03-23 16:12:43 +0100

    Add margin to svg

--- todoReactContext/src/components/Task.tsx
+++ todoReactContext/src/components/Task.tsx
@@ -45,7 +45,7 @@ export const Task = ({
   return (
 
   <div
-    className="m-10 text-xl sm:text-2xl text-backgroundLight dark:text-darkGreen flex min-w-0 basis-100 justify-between border-black dark:border-creamGreen border shadow-xs shadow-darkGreen rounded-md bg-mediumDarkGreen dark:bg-creamGreen p-5"
+    className="m-10 text-2xl text-backgroundLight dark:text-darkGreen flex min-w-0 basis-100 justify-between border-black dark:border-creamGreen border shadow-xs shadow-darkGreen rounded-md bg-mediumDarkGreen dark:bg-creamGreen p-5"
   >
     <div
       className={`flex min-w-0 basis-100 items-center flex-row ${objectEditBoolean ? "hidden" : "block"}`}
@@ -60,17 +60,17 @@ export const Task = ({
       >
       </button>
       <h1
-        className="min-w-0 basis-40 sm:basis-50 grow wrap-break-word ml-5"
+        className="min-w-0 basis-40 sm:basis-50 text-xl sm:text-2xl grow wrap-break-word ml-5"
         style={{textDecorationLine:`${taskObjectDone ? "line-through" : ""}`}}
       >
         {description}
       </h1>
     </div>
     <div
-      className={`flex w-full min-w-0 basis-10 justify-center items-center mr-2 gap-2 sm:gap-5 ${objectEditBoolean ? "hidden" : "block"}`}
+      className={`flex w-full min-w-0 basis-10 justify-center items-center mr-2 ml-5 gap-2 sm:gap-5 ${objectEditBoolean ? "hidden" : "block"}`}
     >
       <button
-        className="cursor-pointer duration-300 ease-out  hover:scale-120"
+        className="cursor-pointer duration-300 ease-out hover:scale-120"
         onClick={() => {
           setEditBoolean(!editBoolean)
           objectEdit(true)
@@ -111,9 +111,7 @@ export const Task = ({
           className="cursor-pointer duration-300 ease-out hover:scale-120 mr-2 ml-2"
           type="submit"
         >
-          <FiCheckSquare
-            className="w-8 h-8"
-          />
+          <FiCheckSquare/>
         </button>
       </form>
     </div>
