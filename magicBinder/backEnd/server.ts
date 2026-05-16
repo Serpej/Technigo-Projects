a4f174f0b824263d1df1e28a9356ed1462209e1f
Serpej <jehag@live.se>
2026-05-16 09:54:53 +0200

    add userRoute

--- magicBinder/backEnd/server.ts
+++ magicBinder/backEnd/server.ts
@@ -2,15 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import { User } from "./models/User";
-
+import { userRouter } from "./routes/userRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use("/users", userRouter);
 
 const mongo_url = process.env.MONGO_URL as string || "mongodb://localhost/magic-binder";
 
@@ -20,28 +19,8 @@ mongoose
   .catch((error) => console.error(`MongoDB connection error`, error));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
-});
-
-app.post("/users", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const salt = await bcrypt.genSalt();
-    const user = new User({name, email, password: await bcrypt.hash(password, salt)});
-    await user.save();
-    res.status(201).json({
-      success: true,
-      message: "User created",
-      id: user._id,
-      accessToken: user.accessToken,
-    });
-  } catch (error) {
-      res.status(400).json({
-      success: false,
-      message: "Could not create user",
-      errors: error
-    });
-  };
+  console.log("Hello Terminal")
+  res.status(200).json({"message": "Root Path"});
 });
 
 app.listen(PORT, () => {
