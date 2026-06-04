c60e8c5837746d33d8c6a838eecdcfdda69332e6
Serpej <jehag@live.se>
2026-06-04 11:37:48 +0200

    Add useState and onChange function to Login

--- magicBinder/frontEnd/src/pages/Login.tsx
+++ magicBinder/frontEnd/src/pages/Login.tsx
@@ -1,16 +1,39 @@
+import { useState } from "react"
+
 export const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleValue = (
+    event: React.ChangeEvent<HTMLInputElement>, 
+    setUseState: React.Dispatch<React.SetStateAction<string>>):void => {
+    const text = event.currentTarget.value;
+    setUseState(text);
+  }
   return(
     <div>
       <h3>Login</h3>
       <section>
         <form action="">
-          <label htmlFor="">
+          <label htmlFor="email">
             Email:
-            <input type="email" name="Email" required/>
+            <input 
+              type="email" 
+              name="Email"
+              id="email"
+              onChange = {(e) => handleValue(e, setEmail)} 
+              required
+            />
           </label>
-          <label htmlFor="">
+          <label htmlFor="password">
             Password:
-            <input type="password" name="Password" required/>
+            <input 
+              type="password" 
+              name="Password"
+              id="password"
+              onChange = {(e) => handleValue(e, setPassword)} 
+              required
+            />
           </label>
         </form>
       </section>
