99bf6fa6639647ab0e155da27684fb07b01e408c
Serpej <jehag@live.se>
2026-07-03 00:04:10 +0200

    Add srcSet and sizes
    Had claude help me calculate sizes based on tail-
    wind responsive design + natural sceen width
    through document.querySelector('img').naturalWidth

--- magicBinder/frontEnd/src/pages/CardSearchResults.tsx
+++ magicBinder/frontEnd/src/pages/CardSearchResults.tsx
@@ -23,7 +23,7 @@ export const CardSearchResults = () => {
 
   return (
     <div
-      className="grid grid-cols-5 gap-4 m-10 mx-40 content-center justify-center"
+      className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 m-10 gap-6 mx-auto content-center justify-center  max-w-[60%]"
     >
         {cards.map((card:ScryfallCard) => {
           return (
@@ -38,8 +38,10 @@ export const CardSearchResults = () => {
                 {card.name}
               </span>
               <img
-                className="rounded-[4.75%_/_3.5%]"
-                src={card.image_uris.small}
+                className="rounded-[4.75%/3.5%]"
+                src={card.image_uris.normal}
+                srcSet={`${card.image_uris.small} 146w, ${card.image_uris.normal} 488w, ${card.image_uris.large} 672w`}
+                sizes="(max-width: 767px) 30vw, (max-width: 1023px) 20vw, 12vw"
                 alt={card.name}
               />
             </div>
