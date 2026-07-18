c8df9fb0b2752300286f52c7357f233943e78968
Serpej <jehag@live.se>
2026-07-18 09:10:17 +0200

    Edit profilePage to not cover Searchbar

--- magicBinder/frontEnd/src/components/ProfilePage.tsx
+++ magicBinder/frontEnd/src/components/ProfilePage.tsx
@@ -16,22 +16,40 @@ export const ProfilePage = () => {
   useEffect(() => {
 
     const fetchData = async () => {
-    
-      const result = await fetchBindersResponse(accessToken);
+      
+      try {
+        setIsLoading(true);
+        const result = await fetchBindersResponse(accessToken);
+        
+        if(!result || !result.success ) {
+          setIsLoading(false);
+          return
+        }
 
-      setIsLoading(true);
-
-      if(!result || !result.success ) {
+        setBinders(result.binderObjects);
+      }finally {
         setIsLoading(false);
-        return
       }
-
-      setBinders(result.binderObjects);
-      setIsLoading(false);
+      
     };
 
     fetchData();
   }, [accessToken]);
+
+  const renderBinder = ():React.ReactNode => {
+    if(isLoading) {
+      return <div>...Loading</div>
+      
+    }else if(binders.length === 0 && !isLoading) {
+      return null
+    } else {
+      return binders.map((binder) => 
+        <div>
+          {binder.name}
+        </div>
+      )
+    }
+  }
 
   return(
     <div
@@ -48,9 +66,8 @@ export const ProfilePage = () => {
             src={deltaBackground}
             alt="A beautiul view of a delta landscape in dusk."
           />
-      </div>
-      <div
-        className="grid col-start-1 row-start-1 grid-cols-[70vw] grid-rows-[70vh] place-content-center"
+          <div
+        className="grid col-start-1 row-start-2 grid-cols-[70vw] grid-rows-[70vh] place-content-center"
       >
         <div
           className="grid grid-rows-[auto_1fr] min-h-0 gap-5  bg-baltic-blue/50 backdrop-blur-sm shadow-2xl p-10 border-2 rounded-sm border-deep-hero-blue "
@@ -67,9 +84,7 @@ export const ProfilePage = () => {
           <div
             className="grid col-start-1 row-start-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
           >
-            {binders.length === 0  && <div>
-              BinderArray goes Here
-            </div>}
+            {renderBinder()}
             <div>
               <button
                 className="cursor-pointer bg-bright-purple/50 hover:bg-bright-purple/70 border-2 border-deep-hero-blue/80 shadow-2xl py-20 px-15 rounded-sm transition delay-80 hover:scale-103 font-medium whitespace-nowrap"
@@ -80,6 +95,8 @@ export const ProfilePage = () => {
           </div>
         </div>
       </div>
+      </div>
+    
     </div>
   )
 }
